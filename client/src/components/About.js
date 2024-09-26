1d7fc0ad32880f14e76ae946ac7655169ce9db4d
Paul Garton <paulgarton1984@hotmail.com>
2024-09-26 20:37:29 +0100

    style and complete layout of about section

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,28 +1,42 @@
 import "../css/about.css";
-import selfPortrait from "../assets/images/harley_hannah.jpg";
+import selfPortrait from "../assets/images/hannah_profile.jpg";
 
 function About() {
   return (
     <section id="about" className="about">
       <div className="aboutInfo">
         <h2>A Bio</h2>
-        <p>
-          Hannah is a talented pencil artist known for their intricate and
-          lifelike drawings, capturing emotion and detail with exceptional
-          precision. Specializing in portraits and nature-inspired
-          illustrations, their work blends realism with a touch of imaginative
-          flair, creating pieces that feel both intimate and timeless. With a
-          deep passion for the craft, Hannah has been honing her skills for
-          several years, mastering shading techniques and the subtle nuances of
-          pencil work. Their dedication and unique artistic voice have earned
-          them a feature in the art book [Book Title], where their work was
-          showcased alongside other emerging artists. Driven by a love for
-          storytelling through imagery, Hannah continues to explore the depths
-          of human expression and natural beauty through the simplicity of
-          pencil on paper.
-        </p>
+        <div className="aboutDescription">
+          <div className="aboutText">
+            <p>
+              Hannah is a talented pencil artist known for her intricate and
+              lifelike drawings, capturing emotion and detail with exceptional
+              precision.
+            </p>
+            <p>
+              Specializing in portraits and nature-inspired illustrations, her
+              work blends realism with a touch of imaginative flair, creating
+              pieces that feel both intimate and timeless.
+            </p>
+            <p>
+              With a deep passion for her craft, Hannah has been honing her
+              skills for several years, mastering shading techniques and the
+              subtle nuances of pencil work. Hannah's dedication and unique
+              artistic voice have earned them a feature in the art book [Book
+              Title], where her work was showcased alongside other emerging
+              artists.
+            </p>
+            <p>
+              Driven by a love for storytelling through imagery, Hannah
+              continues to explore the depths of human expression and natural
+              beauty through the simplicity of pencil on paper.
+            </p>
+          </div>
+          <div className="aboutImage">
+            <img src={selfPortrait} alt="self portrait of the artist" />
+          </div>
+        </div>
       </div>
-      <img src={selfPortrait} alt="self portrait of the artist" />
     </section>
   );
 }
