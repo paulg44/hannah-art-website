302d4a419bf5b08d6953e8f658ab6ff6ba78efed
Paul Garton <paulgarton1984@hotmail.com>
2024-09-30 21:15:41 +0100

    started burger menu: need to add bootstrap and responsive divs

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,15 +1,19 @@
 import "../css/footer.css";
+import { FaInstagram } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer id="footer">
       <ul className="footerInfoLinks">
         <li>Shipping information</li>
+        <li>Returns Policy</li>
         <li>Privacy Policy</li>
         <li>Sitemap</li>
       </ul>
       <ul className="socialIcons">
-        <li>Instagram</li>
+        <li>
+          <FaInstagram />
+        </li>
       </ul>
     </footer>
   );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "../css/navbar.css";
 import {
   FaHome,
@@ -5,29 +6,56 @@ import {
   FaUser,
   FaEnvelope,
   FaQuestion,
+  FaBars,
 } from "react-icons/fa";
 
 function Navbar() {
+  const [isBurgerOpen, setIsBurgerOpen] = useState(false);
+
+  function openBurgerMenu() {
+    setIsBurgerOpen(!isBurgerOpen);
+  }
+
+  function closeBurgerMenu() {
+    setIsBurgerOpen(false);
+  }
+
   return (
-    <div className="navbar">
-      <div className="icons">
-        <a href="#home">
-          <FaHome />
-        </a>
-        <a href="#about">
-          <FaUser />
-        </a>
-        <a href="#shop">
-          <FaShoppingCart />
-        </a>
-        <a href="#contact">
-          <FaEnvelope />
-        </a>
-        <a href="#footer">
-          <FaQuestion />
-        </a>
+    <nav className="navbar">
+      {/* Burger Menu */}
+      <div className="burgerMenuBtn" onClick={openBurgerMenu}>
+        <FaBars />
       </div>
-    </div>
+      <div className={`icons ${isBurgerOpen ? "open" : ""}`}>
+        <ul className="iconsList">
+          <li>
+            <a href="#home" onClick={closeBurgerMenu}>
+              <FaHome />
+            </a>
+          </li>
+          <li>
+            <a href="#about" onClick={closeBurgerMenu}>
+              <FaUser />
+            </a>
+          </li>
+          <li>
+            <a href="#shop" onClick={closeBurgerMenu}>
+              <FaShoppingCart />
+            </a>
+          </li>
+          <li>
+            <a href="#contact" onClick={closeBurgerMenu}>
+              <FaEnvelope />
+            </a>
+          </li>
+          <li>
+            <a href="#footer" onClick={closeBurgerMenu}>
+              <FaQuestion />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
