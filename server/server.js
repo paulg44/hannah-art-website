43323b96524d32606d40f7eb0a13c0e3ea38a147
Paul Garton <paulgarton1984@hotmail.com>
2024-10-01 21:10:21 +0100

    change url path in cors policy

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.REACT_APP_PORT;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.REACT_APP_FRONTEND_URL,
     credentials: true,
   })
 );
